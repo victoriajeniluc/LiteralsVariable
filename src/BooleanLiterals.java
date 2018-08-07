72f2763afa23bcbc73ec7c87799ab0cf88f26f17
Victoria Luc <victoriajeniluc@gmail.com>
2018-08-07 00:44:30 -0500

    Create two methods within the booleanLiteral class to verify when a boolean is true and print it out to the console

---
+++ src/BooleanLiterals.java
@@ -0,0 +1,44 @@
+/**
+ * Class to show examples of boolean literals
+ * HOW THE CODE IS RAN:
+
+ *FIRST METHOD TO LOOK AT: checkBoolean()
+    * Line 32
+        * Created a method named checkBoolean() that returns back a string
+    * Line 33
+        * Created a boolean to test with named testBoolean and gave it a data type of a boolean literal (true)
+    * Line 34
+        * Declared a variable named prompt with a data type of a string literal
+    * Line 36
+        * Created a conditional statement to verify testBoolean and return a statement which in this case is prompt
+    * Since the first conditional statement always returns true, I was able to write it as trueFalse without saying: testBoolean == true -- ability to simplify the code
+
+ * THEN NEXT METHOD TO LOOK AT: main()
+    * Line 26&27
+        * Created a method that called another method and set its value to a string literal named results
+    * Line 28
+        * Print out the value of results
+    * Line 29
+        * Print out a boolean literal
+ */
+
+public class BooleanLiterals {
+    public static void main(String[] args) {
+        String results = checkBoolean();
+        System.out.println(results);
+        System.out.println(false);
+    }
+
+    private static String checkBoolean() {
+        boolean testBoolean = true;
+        String prompt;
+
+        if (testBoolean) {
+            prompt = "YAY THIS IS TRUE";
+        } else {
+           prompt = "NO THIS IS NOT TRUE";
+        }
+        return prompt;
+    }
+
+}
