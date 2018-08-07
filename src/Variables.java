d9ff31862c5ce7ea773b0db1089d99db4222f7e1
Victoria Luc <victoriajeniluc@gmail.com>
2018-08-07 02:27:30 -0500

    Create a class for variables

---
+++ src/Variables.java
@@ -0,0 +1,15 @@
+/**
+ * Class to show examples of variables
+ */
+public class Variables {
+    public static void main(String[] args) {
+        int x = 100; // declaration and initialization on the same line
+        char var; // only declaration, no initialization
+        var = 'A'; // initializating var .. assigning value to var
+        String name = "Udayan"; // Reference type declaration and initialization
+
+        System.out.println("The value of x:" + x); // example of concatenation
+        System.out.println("The value of var:" + var); // example of concatenation
+        System.out.println("My name is " + name); // example of concatenation
+    }
+}
