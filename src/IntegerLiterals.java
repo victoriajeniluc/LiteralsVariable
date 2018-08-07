41dd74f9d52cb5b30c5fce8d62ccc2ede0c98268
Victoria Luc <victoriajeniluc@gmail.com>
2018-08-06 23:52:27 -0500

    Initial commit

---
+++ src/IntegerLiterals.java
@@ -0,0 +1,12 @@
+/**
+ * Class to show examples of Integer literal representations of 15. 
+ */
+
+public class IntegerLiterals {
+    public static void main(String[] args) {
+        System.out.println(15); // decimal
+        System.out.println(017); // octal
+        System.out.println(0xF); // hexadecimal
+        System.out.println(0b1111); // binary
+    }
+}
