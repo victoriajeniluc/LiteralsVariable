81b2dcfa13c337e3dce812d7e5a7038b2e3e8128
Victoria Luc <victoriajeniluc@gmail.com>
2018-08-07 01:08:00 -0500

    Create character literals

---
+++ src/characterLiterals.java
@@ -0,0 +1,21 @@
+public class characterLiterals {
+    public static void main(String[] args) {
+        System.out.println('a');
+
+        System.out.print("->");
+        System.out.print("\n");
+        System.out.println("<-");
+
+        System.out.print("->");
+        System.out.print("\t");
+        System.out.println("<-");
+
+        System.out.print("->");
+        System.out.print("\\");
+        System.out.println("<-");
+
+        System.out.print("->");
+        System.out.print("\'");
+        System.out.println("<-");
+    }
+}
